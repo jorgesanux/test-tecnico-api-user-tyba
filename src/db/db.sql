135bf1b2636ce5e0d18f389c288b2175e4744631
Jorge Sanabria <jorgesanux1@gmail.com>
2022-11-03 00:02:06 -0500

    Added email and indexes
    Signed-off-by: Jorge Sanabria <jorgesanux1@gmail.com>

--- src/db/db.sql
+++ src/db/db.sql
@@ -17,6 +17,11 @@ create table history(
     endpoint text not null,
     hostname text not null,
     ip varchar(50) not null,
+    email varchar(80) not null,
     method varchar(20) not null,
     created_at timestamptz default now()::timestamptz
 );
+
+create index history_email on history(email);
+create index history_endpoint on history(endpoint);
+create index history_method on history(method);
