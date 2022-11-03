cf30ff694b14c4c534edcf28c66ac706483935e3
Jorge Sanabria <jorgesanux1@gmail.com>
2022-11-03 01:38:43 -0500

    Added pagination and user agent on history
    Signed-off-by: Jorge Sanabria <jorgesanux1@gmail.com>

--- src/controller/history.controller.js
+++ src/controller/history.controller.js
@@ -5,11 +5,18 @@ import History from "../model/history.model.js";
 export default class HistoryController{
 
     async getHistory(req, res, next){
+        let {page = 1, limit = 10} = req.query;
+
         try{
             const history = await History.findAll({
                 where: {
                     email: req.user.email
-                }
+                },
+                limit,
+                offset: (page-1) * limit,
+                order: [
+                    ["created_at", "DESC"]
+                ]
             });
 
             res.status(200);
@@ -25,9 +32,11 @@ export default class HistoryController{
 
     async saveHistory(req, res, next){
         await History.create({
-            endpoint: req.originalUrl,
+            //Se remueve el token, si este llega por query param
+            endpoint: req.originalUrl.replace(/token=[^&]*/, "token=***"),
             hostname: req.hostname,
             ip: req.ip,
+            agent: req.headers["user-agent"],
             method: req.method,
             email: req.user.email
         });
