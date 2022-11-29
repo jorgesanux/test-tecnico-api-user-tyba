96e13b4ccabf3fcf6bfa7ae944ecc954613a6e1f
Jorge Sanabria <jorgesanux1@gmail.com>
2022-11-29 01:09:54 -0500

    Added auth to logout endpoint
    Signed-off-by: Jorge Sanabria <jorgesanux1@gmail.com>

--- src/routes/auth.router.js
+++ src/routes/auth.router.js
@@ -1,11 +1,12 @@
 import express from "express";
 import AuthController from "../controller/auth.controller.js";
+import authenticator from "../middleware/authenticator.middleware.js";
 
 const authController = new AuthController();
 const router = express.Router();
 
 router.post("/login", authController.login.bind(authController));
-router.post("/logout", authController.logout.bind(authController));
+router.post("/logout", authenticator, authController.logout.bind(authController));
 router.post("/register", authController.register.bind(authController));
 
 export default router;
